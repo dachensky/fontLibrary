e22f4896aa3a57310859e0668fc9465931230da6
grey_wolf <1906885811@qq.com>
2021-06-17 18:24:45 +0800

    done4

--- api/proxy.js
+++ api/proxy.js
@@ -4,23 +4,9 @@ const {
 } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-    if (req.url.startsWith('/api')) {
-        // 创建代理对象并转发请求
+    if (req.url.startsWith('/hellofont')) {
         createProxyMiddleware({
-            target:'https://back3.hellofont.cn',
-            ws: true,
-            changeOrigin: true,
-            pathRewrite: {
-                '^/api': '/'
-            },
-            headers: {
-                referer: "https://www.hellofont.cn/",
-                origin: "https://www.hellofont.cn/",
-            },
-        })(req, res)
-    }else if (req.url.startsWith('/hellofont')){
-        createProxyMiddleware({
-            target:'https://www.hellofont.cn/',
+            target: 'https://www.hellofont.cn/',
             ws: true,
             changeOrigin: true,
             pathRewrite: {
@@ -31,9 +17,23 @@ module.exports = (req, res) => {
                 origin: "https://www.hellofont.cn/font-list",
             },
         })(req, res)
-    }else if(req.url.startsWith('/webfontmanagement')){
+    } else if (req.url.startsWith('/api')) {
+        // 创建代理对象并转发请求
         createProxyMiddleware({
-            target:'https://www.hellowebfont.com/',
+            target: 'https://back3.hellofont.cn',
+            ws: true,
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': '/'
+            },
+            headers: {
+                referer: "https://www.hellofont.cn/",
+                origin: "https://www.hellofont.cn/",
+            },
+        })(req, res)
+    } else if (req.url.startsWith('/webfontmanagement')) {
+        createProxyMiddleware({
+            target: 'https://www.hellowebfont.com/',
             ws: true, //代理websockets
             changeOrigin: true,
             pathRewrite: {
