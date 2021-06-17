450f3bbede92ca1165ee3f771a1fe8f9b996273a
grey_wolf <1906885811@qq.com>
2021-06-17 17:39:39 +0800

    test

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,50 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
+const express = require('express')
 
+const app = express();
+
+//设置跨域访问
+app.all("*", function (req, res, next) {
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin", "*");
+    //允许的header类型
+    res.header("Access-Control-Allow-Headers", "content-type");
+
+    if (req.method.toLowerCase() == "options") res.send(200);
+    //让options尝试请求快速结束
+    else next();
+});
 
 
 module.exports = function (app){
-    app.use(createProxyMiddleware('/api', {target: "https://back3.hellofont.cn"}));
-    app.use(createProxyMiddleware('/hellofont', {target: "https://www.hellofont.cn/"}));
-    app.use(createProxyMiddleware('/webfontmanagement', {target: "https://www.hellowebfont.com/"}));
+    app.use(
+        "/api",
+        createProxyMiddleware({
+            target: "https://back3.hellofont.cn",
+            changeOrigin: true,
+            pathRewrite: {
+                "^/api": "", // rewrite path
+            },
+        })
+    );
+    app.use(
+        "/hellofont",
+        createProxyMiddleware({
+            target: "https://www.hellofont.cn/",
+            changeOrigin: true,
+            pathRewrite: {
+                "^/hellofont": "", // rewrite path
+            },
+        })
+    );
+    app.use(
+        "/webfontmanagement",
+        createProxyMiddleware({
+            target: "https://www.hellowebfont.com/",
+            changeOrigin: true,
+            pathRewrite: {
+                "^/webfontmanagement": "", // rewrite path
+            },
+        })
+    );
 }
