695f38a6d93dcd4bd81fa78626adf9259ef11287
grey_wolf <1906885811@qq.com>
2021-06-17 17:59:44 +0800

    done

--- api/proxy.js
+++ api/proxy.js
@@ -13,7 +13,11 @@ module.exports = (req, res) => {
                 // 通过路径重写，去除请求路径中的 `/backend`
                 // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
                 '^/api': '/'
-            }
+            },
+            headers: {
+                referer: "https://www.hellofont.cn/",
+                origin: "https://www.hellofont.cn/",
+            },
         })(req, res)
     }
 
@@ -25,7 +29,11 @@ module.exports = (req, res) => {
                 // 通过路径重写，去除请求路径中的 `/backend`
                 // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
                 '^/hellofont': '/'
-            }
+            },
+            headers: {
+                referer: "https://www.hellofont.cn",
+                origin: "https://www.hellofont.cn/font-list",
+            },
         })(req, res)
     }
 
@@ -37,7 +45,11 @@ module.exports = (req, res) => {
                 // 通过路径重写，去除请求路径中的 `/backend`
                 // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
                 '^/webfontmanagement': '/'
-            }
+            },
+            headers: {
+                referer: "https://www.hellofont.cn",
+                origin: "https://www.hellofont.cn/",
+            },
         })(req, res)
     }
 
